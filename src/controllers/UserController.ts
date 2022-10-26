6a2f290e834e4e38b36733af0c315ef9843bfb95
jonveigel <cloudeca1@gmail.com>
2022-10-26 20:55:13 -0300

    userController/get one by id implements

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { User } from "../entity/User";
 import { userRepository } from "../repositories/userRepository";
 
 export class UserController {
@@ -6,5 +7,19 @@ export class UserController {
         const users = await userRepository.find({ select: ['iduser', 'name', 'email', 'apartment', 'userphoto'] });
 
         return res.send(users)
+    }
+
+    async getOneById(req: Request, res: Response) {
+        const iduser: number = parseInt(req.params.iduser, 10);
+
+        let user: User;
+
+        try {
+            user = await userRepository.findOneByOrFail({ iduser });
+        } catch (error) {
+            return res.status(404).send("User not found :X");
+        }
+
+        return res.send(user);
     }
 }
