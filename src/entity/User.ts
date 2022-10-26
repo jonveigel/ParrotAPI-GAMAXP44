419b887d7659a70dc13f6ee8db92f76ed090f378
jonveigel <cloudeca1@gmail.com>
2022-10-26 17:57:02 -0300

    user entity created

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,48 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { IsNotEmpty, Length } from "class-validator"
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Unique } from "typeorm"
+import bcrypt from "bcryptjs";
+
 
 @Entity()
+@Unique(["email"])
 export class User {
 
     @PrimaryGeneratedColumn()
-    id: number
+    iduser: number
 
     @Column()
-    firstName: string
+    @Length(4, 70)
+    name: string
+
+    @Column({ unique: true })
+    @Length(4, 70)
+    email: string
 
     @Column()
-    lastName: string
+    @Length(5, 120)
+    password: string
 
     @Column()
-    age: number
+    @Length(0, 255)
+    userphoto: string
 
+    @Column()
+    @IsNotEmpty()
+    role: string
+
+    @Column()
+    @CreateDateColumn()
+    createdAt: Date
+
+    @Column()
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    passwordHash() {
+        this.password = bcrypt.hashSync(this.password, 10)
+    }
+
+    UnencryptedPassword(UnencryptedPassword: string) {
+        return bcrypt.compareSync(UnencryptedPassword, this.password)
+    }
 }
