9891ce7976bd7a2db5dc7e3153d579c7b9332c03
jonveigel <cloudeca1@gmail.com>
2022-10-26 18:17:25 -0300

    dotenv config/ mysql connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,20 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import "dotenv";
+import { DataSource, DataSourceOptions } from "typeorm";
+import { SeederOptions } from "typeorm-extension";
 
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+
+const port = process.env.DB_PORT as number | undefined
+
+const options: DataSourceOptions & SeederOptions = {
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: port,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: [`${__dirname}/**/entity/*.{ts,js}`],
+    migrations: [`${__dirname}/**/migrations/*.{ts,js}`]
+}
+
+export const AppDataSource = new DataSource(options)
