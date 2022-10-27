5ab370ebb9625c38e0e79b42374d8903d1cafc99
jonveigel <cloudeca1@gmail.com>
2022-10-26 22:06:53 -0300

    jwt middleware config test

---
+++ src/middleware/JwtMiddleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import config from "../config/config";
+import jwt from "jsonwebtoken";
+
+export const jwtMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
+
+    if(!authorization) {
+        return res.status(403).json({ message: "User not Login! :(" });
+    }
+
+    const token = authorization.split(" ")[1];
+
+    let jwtPayLoad;
+
+    try {
+        jwtPayLoad = <any>jwt.verify(token, config.jwtSecret)
+    } catch (error) {
+        res.status(401).send;
+    }
+
+    const { iduser, email } = jwtPayLoad;
+    const newToken = jwt.sign({ iduser, email }, config.jwtSecret, {expiresIn: "24h"});
+
+    res.setHeader("token", newToken);
+
+    next();
+}
