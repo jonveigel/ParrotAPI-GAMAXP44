d0f4551dc78013e8f17536ebf126805e7943d91e
jonveigel <cloudeca1@gmail.com>
2022-10-26 22:18:13 -0300

    posts routes created

---
+++ src/routes/post.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { PostController } from "../controllers/PostController";
+import { jwtMiddleware } from "../middleware/JwtMiddleware";
+
+const router = Router();
+
+router.post("/", jwtMiddleware, new PostController().create);
+router.get("/", jwtMiddleware, new PostController().listAll);
+router.get("/myposts", jwtMiddleware, new PostController().listAllByUserId);
