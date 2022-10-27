3535b101c2f58cc92568443d0e500d8426aa3947
jonveigel <cloudeca1@gmail.com>
2022-10-26 21:12:17 -0300

    userController/editUser implements

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,7 @@
 import { validate } from "class-validator";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import config from "../config/config";
 import { User } from "../entity/User";
 import { userRepository } from "../repositories/userRepository";
 
@@ -54,5 +56,44 @@ export class UserController {
         }
 
         return res.status(201).send("User Created!!!!!!!");
+    }
+
+    async editUser(req: Request, res: Response) {
+        const { authorization } = req.headers;
+        if(!authorization) {
+            return;
+        }
+
+        const token = authorization.split(" ")[1];
+
+        let iduser;
+
+        try {
+            const jwtPayLoad = <any>jwt.verify(token, config.jwtSecret);
+            iduser = jwtPayLoad.userId;
+        } catch (error) {
+            return res.status(401).send;
+        }
+
+        const { name, email, apartment, userphoto } = req.body;
+
+        let user: User = await userRepository.findOneByOrFail({ iduser });
+        if(name) user.name = name
+        if(email) user.email = email
+        if(apartment) user.apartment = apartment
+        if(userphoto) user.userphoto = userphoto
+
+        const errors = await validate(user);
+        if(errors.length > 0) {
+            return res.send(400).send(errors)
+        }
+
+        try {
+            await userRepository.save(user)
+        } catch (error) {
+            return res.status(400);
+        }
+
+        return res.status(201).send("User edited successfully!! :)");
     }
 }
